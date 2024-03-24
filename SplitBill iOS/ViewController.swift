cd5b7fc38386092dc21677d03751e90c6157553c
iiTzAman <aman.giri2055@gmail.com>
2024-03-24 18:08:29 -0400

    Initial Commit

---
+++ SplitBill iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplitBill iOS
+//
+//  Created by Aman Giri on 2024-03-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
